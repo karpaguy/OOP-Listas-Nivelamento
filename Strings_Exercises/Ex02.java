e26ee1956d132458f95a404615e69434093396ba
karpaguy <denuzzidouglas@gmail.com>
2026-03-23 13:08:28 -0300

    StringExercises02

---
+++ Strings_Exercises/Ex02.java
@@ -0,0 +1,15 @@
+package br.edu.ifsp.Strings_Exercises;
+
+import java.util.Scanner;
+
+public class Ex02 {
+    public static void main() {
+        final Scanner scanner = new Scanner(System.in);
+        final String phrase = scanner.nextLine();
+        final String word = scanner.nextLine();
+
+        System.out.println(Ex02WordCount.wordTotalInPhrase(phrase, word));
+    }
+}
+
+// Crie uma classe com um método que conte quantas vezes uma palavra aparece em uma frase.
