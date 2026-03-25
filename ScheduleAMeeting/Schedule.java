86320499fe838b1aaf4b567a101c04f70cd62942
karpaguy <denuzzidouglas@gmail.com>
2026-03-25 15:05:40 -0300

    Refactor to be Array

--- ScheduleAMeeting/Schedule.java
+++ ScheduleAMeeting/Schedule.java
@@ -9,37 +9,37 @@ public class Schedule {
     private LocalDate day;
     private LocalTime startTime;
     private LocalTime endTime;
-    private List<Meeting> scheduledMeetings;
+    private final Meeting[] scheduledMeetings;
+    private int meetingAcc = 0;
 
     public Schedule(LocalDate day, LocalTime startTime, LocalTime endTime) {
         this.day = day;
         this.startTime = startTime;
         this.endTime = endTime;
-        this.scheduledMeetings = new ArrayList<>();
+        this.scheduledMeetings = new Meeting[10];
     }
 
     public void addMeeting(Meeting meeting) {
         if (meeting == null) return;
         if (meeting.getStartTime().isBefore(this.startTime) || meeting.getEndTime().isAfter(this.endTime)) {
-            System.out.println("Meeting out of schedule");
             return;
         }
-
-        for (Meeting m : scheduledMeetings) {
-            if (meeting.getStartTime().isBefore(m.getEndTime())) {
-                System.out.println("Meeting time conflicting with another meeting.");
+        for (int i = 0; i < scheduledMeetings.length; i++) {
+            if (meeting.getStartTime().isBefore(scheduledMeetings[i].getEndTime())) {
                 return;
             }
         }
-        scheduledMeetings.add(meeting);
+        scheduledMeetings[meetingAcc++] = (meeting);
     }
 
     public String scheduleAsString() {
         StringBuilder sb = new StringBuilder();
         sb.append("Schedule ( ").append(day).append(" )\n");
         sb.append("Starting Time: ").append(startTime).append("| ").append("Ending Time: ").append(endTime).append("\n\n");
-        for (Meeting m : scheduledMeetings) {
-            sb.append("Time: ").append(m.getStartTime()).append(" ~ ").append(m.getEndTime()).append(", Reason: ").append(m.getDescription()).append("\n");
+        for (int i = 0; i < meetingAcc; i++) {
+            sb.append("Time: ").append(scheduledMeetings[i].getStartTime())
+                    .append(" ~ ").append(scheduledMeetings[i].getEndTime())
+                    .append(", Reason: ").append(scheduledMeetings[i].getDescription()).append("\n");
         }
         return sb.toString();
     }
