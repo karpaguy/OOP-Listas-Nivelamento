39c2d8857a8043355b37b602b7083b1ca559969d
karpaguy <denuzzidouglas@gmail.com>
2026-03-27 13:31:15 -0300

    Main specs done

--- Simulados/P1/Main.java
+++ Simulados/P1/Main.java
@@ -2,6 +2,23 @@ package br.edu.ifsp.Simulados.P1;
 
 public class Main {
     public static void main(String args[]) {
+        UserAccount Teobaldo = new UserAccount("teobaldo@gmail.com", "teobaldo28");
+        UserAccount Leopoldo = new UserAccount("leopoldo@gmail.com", "XxleopoldoxX");
 
+        Teobaldo.acceptFollower(Leopoldo);
+        Teobaldo.publish("Treinar costas é muito legal, meus manos.");
+        Teobaldo.publish("Treinar pernas é muito legal, meus manos.");
+        Teobaldo.delete(1);
+        Leopoldo.clapPost(1);
+        Leopoldo.clapPost(1);
+        Teobaldo.publish("Treinar pernas é muito TOP, meus manos.");
+
+        System.out.println(Leopoldo.showTimeline());
+        System.out.println("-----");
+
+        Leopoldo.booPost(2);
+        Teobaldo.blockFollower(Leopoldo);
+        Teobaldo.publish("To pobre.");
+        System.out.println(Leopoldo.showTimeline());
     }
 }
