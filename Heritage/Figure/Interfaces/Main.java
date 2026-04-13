93531174f700801cdf7c2580b6d7c86335db97bd
karpaguy <denuzzidouglas@gmail.com>
2026-04-12 21:43:11 -0300

    Figures Implemented

---
+++ Heritage/Figure/Interfaces/Main.java
@@ -0,0 +1,17 @@
+package br.edu.ifsp.Heritage.Figure.Interfaces;
+
+public class Main {
+    public void main(String[] args) {
+        Figure rectangle = new Rectangle(4, 2);
+        if (rectangle instanceof Figure) {
+            System.out.println("WORKS!");
+        }
+
+        // Funciona de ambas as formas.
+        Rectangle rectangle2 = new Rectangle(10, 5);
+        System.out.println(rectangle2.getFigureName());
+
+        Figure triangle = new Triangle(3, 4, 5);
+        System.out.println(triangle.getArea());
+    }
+}
